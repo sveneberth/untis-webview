89155894a1bf9493a449887ad6f0ffc94f9fed8f
Sven Eberth <Sven.Eberth@gmail.com>
2018-01-21 03:38:04 +0100

    minor details

--- modules/vertretung/layout_view.php
+++
@@ -1,9 +0,0 @@
-#if(show_meta_info):
-<span class="meta-info"><?= __('writtenInfo', $author, $date, $time) ?></span>
-#endif
-<img class="thumbnail-img" src="{{post_imageURL}}" title="{{post_imageTitle}}" alt="{{post_imageTitle}}">
-{{post_content}}
-
-<br>
-<br>
-<a href="{{MAIN_URL}}/posts/list">&raquo;<?= __('gotoPostList') ?></a>
